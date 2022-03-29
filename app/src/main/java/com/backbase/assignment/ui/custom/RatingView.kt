dee5f2fea036b04a560738d0e64a8565a35e4a5a
Anibal Bastias Soto <anibal.bastias@gmail.com>
2022-03-29 15:48:10 -0400

    Update dependencies

---
+++ app/src/main/java/com/backbase/assignment/ui/custom/RatingView.kt
@@ -0,0 +1,9 @@
+package com.backbase.assignment.ui.custom
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class RatingView @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : View(context, attrs, defStyleAttr)
