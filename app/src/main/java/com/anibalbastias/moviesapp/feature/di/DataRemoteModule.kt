64508a9b3d64b2bd74068ee0f82986ee7bbffd4f
Anibal Bastias Soto <anibal.bastias@gmail.com>
2022-04-23 09:50:08 -0400

    Implement certification Pinning

--- app/src/main/java/com/anibalbastias/moviesapp/feature/di/DataRemoteModule.kt
+++ app/src/main/java/com/anibalbastias/moviesapp/feature/di/DataRemoteModule.kt
@@ -13,11 +13,16 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import okhttp3.CertificatePinner
+import okhttp3.ConnectionSpec
 import okhttp3.OkHttpClient
+import okhttp3.TlsVersion
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.*
 import javax.inject.Singleton
+
 
 @InstallIn(SingletonComponent::class)
 @Module
@@ -43,21 +48,51 @@ object DataRemoteModule {
 
     @Singleton
     @Provides
-    fun provideRetrofit(moshi: MoshiConverterFactory, context: Context): Retrofit.Builder {
+    fun provideCertificatePinner(): CertificatePinner {
+        return CertificatePinner.Builder()
+            .add(BuildConfig.API_HOST, BuildConfig.API_PUBLIC_KEY_HASH)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideTLSConnection(): ConnectionSpec {
+        return ConnectionSpec.Builder(ConnectionSpec.MODERN_TLS)
+            .tlsVersions(TlsVersion.TLS_1_2)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideOkHttp(
+        context: Context,
+        certificatePinner: CertificatePinner,
+        connectionSpec: ConnectionSpec,
+    ): OkHttpClient {
+        return OkHttpClient.Builder()
+            .certificatePinner(certificatePinner)
+            .connectionSpecs(Collections.singletonList(connectionSpec))
+            .addInterceptor(
+                HttpLoggingInterceptor().apply {
+                    level = HttpLoggingInterceptor.Level.BODY
+                }
+            )
+            .addInterceptor(SecurityInterceptor())
+            .addInterceptor(ConnectionInterceptor(context))
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(
+        moshi: MoshiConverterFactory,
+        okHttpClient: OkHttpClient,
+        context: Context,
+    ): Retrofit.Builder {
         return Retrofit.Builder()
             .baseUrl(BuildConfig.API_BASE_URL)
             .addConverterFactory(moshi)
-            .client(
-                OkHttpClient.Builder()
-                    .addInterceptor(
-                        HttpLoggingInterceptor().apply {
-                            level = HttpLoggingInterceptor.Level.BODY
-                        }
-                    )
-                    .addInterceptor(SecurityInterceptor())
-                    .addInterceptor(ConnectionInterceptor(context))
-                    .build()
-            )
+            .client(okHttpClient)
     }
 
     @Singleton
