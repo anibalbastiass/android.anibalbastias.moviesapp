163d78ff1bd789ea9463ee119bc0f15d8dd1d695
Anibal Bastias Soto <anibal.bastias@gmail.com>
2022-04-25 10:22:01 -0400

    Improve security credentials.

--- app/src/main/java/com/anibalbastias/moviesapp/feature/ui/screens/favorites/FavoritesScreen.kt
+++ app/src/main/java/com/anibalbastias/moviesapp/feature/ui/screens/favorites/FavoritesScreen.kt
@@ -2,13 +2,22 @@ package com.anibalbastias.moviesapp.feature.ui.screens.favorites
 
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.rememberLottieComposition
 import com.anibalbastias.moviesapp.R
 import com.anibalbastias.moviesapp.feature.presentation.viewmodel.MoviesViewModel
 import com.anibalbastias.moviesapp.feature.ui.screens.movies.list.StickyHeaderMovie
@@ -30,5 +39,22 @@ fun FavoritesScreen(
         stickyHeader {
             StickyHeaderMovie(title = stringResource(id = R.string.favorites))
         }
+
+        item {
+            Box(modifier = Modifier
+                .width(200.dp)
+                .height(200.dp)) {
+                Loader()
+            }
+        }
     }
+}
+
+@Composable
+fun Loader() {
+    val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.movie))
+    LottieAnimation(
+        composition = composition,
+        iterations = LottieConstants.IterateForever
+    )
 }
