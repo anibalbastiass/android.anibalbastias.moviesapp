fb85faab218f678b7ca74a2cb298e5981cb8afae
Anibal Bastias Soto <anibal.bastias@gmail.com>
2022-04-25 15:58:38 -0400

    Add Empty list screen for favorites

--- app/src/main/java/com/anibalbastias/moviesapp/feature/ui/screens/favorites/FavoritesScreen.kt
+++ app/src/main/java/com/anibalbastias/moviesapp/feature/ui/screens/favorites/FavoritesScreen.kt
@@ -2,17 +2,17 @@ package com.anibalbastias.moviesapp.feature.ui.screens.favorites
 
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.airbnb.lottie.compose.LottieAnimation
 import com.airbnb.lottie.compose.LottieCompositionSpec
@@ -21,6 +21,7 @@ import com.airbnb.lottie.compose.rememberLottieComposition
 import com.anibalbastias.moviesapp.R
 import com.anibalbastias.moviesapp.feature.presentation.viewmodel.MoviesViewModel
 import com.anibalbastias.moviesapp.feature.ui.screens.movies.list.StickyHeaderMovie
+import com.anibalbastias.uikitcompose.components.atom.Body1
 import com.anibalbastias.uikitcompose.utils.rememberForeverLazyListState
 
 @ExperimentalFoundationApi
@@ -28,25 +29,73 @@ import com.anibalbastias.uikitcompose.utils.rememberForeverLazyListState
 fun FavoritesScreen(
     moviesViewModel: MoviesViewModel = hiltViewModel(),
 ) {
-    val lazyListState = rememberForeverLazyListState(key = "Favorites")
+    EmptyFavoriteScreen()
 
-    LazyColumn(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(colorResource(id = R.color.backgroundColor)),
-        state = lazyListState
+//    val lazyListState = rememberForeverLazyListState(key = "Favorites")
+//
+//    LazyColumn(
+//        modifier = Modifier
+//            .fillMaxSize()
+//            .background(colorResource(id = R.color.backgroundColor)),
+//        state = lazyListState
+//    ) {
+//        stickyHeader {
+//            StickyHeaderMovie(title = stringResource(id = R.string.favorites))
+//        }
+//
+//        item {
+//
+//        }
+//    }
+}
+
+@Composable
+fun EmptyFavoriteScreen() {
+    ConstraintLayout(modifier = Modifier
+        .padding(bottom = 50.dp)
+        .fillMaxSize()
+        .background(colorResource(id = R.color.backgroundColor))
     ) {
-        stickyHeader {
-            StickyHeaderMovie(title = stringResource(id = R.string.favorites))
+        val (circle, loader, text) = createRefs()
+
+        Box(
+            modifier = Modifier
+                .constrainAs(circle) {
+                    top.linkTo(parent.top)
+                    bottom.linkTo(parent.bottom)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                }
+                .clip(CircleShape)
+                .background(colorResource(id = R.color.white))
+                .size(120.dp)
+        )
+
+        Box(
+            modifier = Modifier
+                .size(100.dp)
+                .constrainAs(loader) {
+                    top.linkTo(parent.top)
+                    bottom.linkTo(parent.bottom)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                }
+                .padding(start = 20.dp)
+        ) {
+            Loader()
         }
 
-        item {
-            Box(modifier = Modifier
-                .width(200.dp)
-                .height(200.dp)) {
-                Loader()
-            }
-        }
+        Body1(
+            text = stringResource(id = R.string.favorites_empty),
+            color = colorResource(id = R.color.textColor),
+            modifier = Modifier
+                .padding(top = 30.dp)
+                .constrainAs(text) {
+                    top.linkTo(loader.bottom)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                }
+        )
     }
 }
 
